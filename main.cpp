baf0612bcfef8f0240b6e1b0781fafe57eb12f89
Jacques Erasmus <jacques.erasmus@ecampty.co.za>
2018-10-31 09:39:43 +0200

    two dma dacs in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 #include "libTest/libTest.h"
 #include "ConstalationDiagrams/ModulationMaping.h"
-#include "ADC_Dma.h"
+//#include "ADC_Dma.h"
 
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
@@ -29,30 +29,38 @@ DigitalOut led3(LED3);
 #define DACx_RELEASE_RESET()            __HAL_RCC_DAC_RELEASE_RESET()
 
 /* Definition for DACx Channel Pin */
-#define DACx_CHANNEL_PIN                GPIO_PIN_4
-#define DACx_CHANNEL_GPIO_PORT          GPIOA
+#define DACx_CHANNEL_PIN_I                GPIO_PIN_5
+#define DACx_CHANNEL_PIN_Q                GPIO_PIN_4
+#define DACx_CHANNEL_GPIO_PORT            GPIOA
 
 /* Definition for DACx's Channel */
 #define DACx_DMA_CHANNEL                DMA_CHANNEL_7
 
-#define DACx_CHANNEL                    DAC_CHANNEL_1
+#define DACx_CHANNEL_I                    DAC_CHANNEL_2
+#define DACx_CHANNEL_Q                    DAC_CHANNEL_1
 
 /* Definition for DACx's DMA_STREAM */
-#define DACx_DMA_INSTANCE               DMA1_Stream5
+#define DACx_DMA_INSTANCE_I               DMA1_Stream6
+#define DACx_DMA_INSTANCE_Q               DMA1_Stream5
 
 /* Definition for DACx's NVIC */
-#define DACx_DMA_IRQn                   DMA1_Stream5_IRQn
-#define DACx_DMA_IRQHandler             DMA1_Stream5_IRQHandler
+#define DACx_DMA_IRQn_I                  DMA1_Stream6_IRQn
+#define DACx_DMA_IRQn_Q                  DMA1_Stream5_IRQn
+
+#define DACx_DMA_IRQHandler_I             DMA1_Stream6_IRQHandler
+#define DACx_DMA_IRQHandler_Q             DMA1_Stream5_IRQHandler
 
 
 #define SYMBOLS_PERTRANSFER 100
 
-uint8_t DataToTransmit [SYMBOLS_PERTRANSFER] ;
+uint8_t DataToTransmit_I [SYMBOLS_PERTRANSFER] ;
+uint8_t DataToTransmit_Q [SYMBOLS_PERTRANSFER] ;
 
 
-
-DAC_HandleTypeDef    DacHandle;
-static DAC_ChannelConfTypeDef DACsConfig;
+DAC_HandleTypeDef    DacHandleI;
+DAC_HandleTypeDef    DacHandleQ;
+static DAC_ChannelConfTypeDef DACsConfigI;
+static DAC_ChannelConfTypeDef DACsConfigQ;
 //const uint8_t aEscalator8bit[6] = {0x0, 0x33, 0x66, 0x99, 0xCC, 0xFF};
  
 __IO uint8_t ubSelectedWavesForm = 1;
@@ -87,9 +95,9 @@ __IO uint8_t ubKeyPressed = SET;
 
 
 
-extern  __IO uint32_t transferErrorDetected; /* Set to 1 if an error transfer is detected */
-/* Variable used to get converted value */
-extern __IO uint32_t uhADCxConvertedValue[1000] ;
+// extern  __IO uint32_t transferErrorDetected; /* Set to 1 if an error transfer is detected */
+// /* Variable used to get converted value */
+// extern __IO uint32_t uhADCxConvertedValue[1000] ;
 
 
 static bool bPrint = false;
@@ -101,14 +109,14 @@ void flip() {
 
 Ticker OutTimmer;
 
-extern __IO uint32_t conversionCompleteConter ;
+// extern __IO uint32_t conversionCompleteConter ;
 
 uint16_t analogOutCount = 0;
 int main() {
     pc.baud(115200);
     pc.printf("Startup \n\r");
     
-    AdcDma  AdcDmaInstance;
+    //AdcDma  AdcDmaInstance;
  
     led1 = 1;
     led2 = 1;
@@ -126,16 +134,20 @@ int main() {
      
     for (size_t i = 0; i < halfway; i++) //0 - 50
     {
-       DataToTransmit[i] = i * incPerHalf;
+       DataToTransmit_I[i] = i * incPerHalf;
+       DataToTransmit_Q[i] = i * incPerHalf;
     }
 
     for (size_t i = halfway; i < SYMBOLS_PERTRANSFER; i++) //51 - 99
     {
-       DataToTransmit[i] = DataToTransmit[i-1]- incPerHalf;
+       DataToTransmit_I[i] = DataToTransmit_I[i-1]- incPerHalf;
+       DataToTransmit_Q[i] = DataToTransmit_Q[i-1]- incPerHalf;
+       
     }
 
 
-    DacHandle.Instance = DACx;
+    DacHandleI.Instance = DACx;
+    DacHandleQ.Instance = DACx;
     TIM6_Config();
 
 
@@ -159,24 +171,24 @@ int main() {
 
     OutTimmer.attach_us(&flip,1000000);
 
-    transferErrorDetected = 0;
+    //transferErrorDetected = 0;
 
 
-    AdcDmaInstance.DMA_Config();
+    //AdcDmaInstance.DMA_Config();
 
     while (true) 
     {
          //pc.printf("poes ");
         //A_out0.write_u16(analogOutCount);
-        analogOutCount += 270;
-        if (transferErrorDetected == 1)
-        {
-          /* Turn LED2 on*/
-            led2 =1;
-            pc.printf("Transfer Error detected ********* \n\r");
-            wait(1);
-            transferErrorDetected = 0;
-        }
+        //analogOutCount += 270;
+        // if (transferErrorDetected == 1)
+        // {
+        //   /* Turn LED2 on*/
+        //     led2 =1;
+        //     pc.printf("Transfer Error detected ********* \n\r");
+        //     wait(1);
+        //     transferErrorDetected = 0;
+        // }
         if(bPrint)
         {
           bPrint =false;
@@ -184,7 +196,7 @@ int main() {
 
 
           pc.printf("print ");
-          pc.printf("v : %i c : %i \n\r",uhADCxConvertedValue[0],conversionCompleteConter);
+         // pc.printf("v : %i c : %i \n\r",uhADCxConvertedValue[0],conversionCompleteConter);
 
 
 
@@ -237,24 +249,42 @@ void TIM6_Config(void)
 static void DAC_Ch1_EscalatorConfig(void)
 {
   /*##-1- Initialize the DAC peripheral ######################################*/
-  if (HAL_DAC_Init(&DacHandle) != HAL_OK)
+  if (HAL_DAC_Init(&DacHandleI) != HAL_OK)
   {
     /* Initialization Error */
     led2 = 1;
-    pc.printf(" DAC Initialization  ######################################### Error");
+    pc.printf(" DAC Initialization I  ######################################### Error");
+    wait(1);
+    while(1){};
+  }
+  if (HAL_DAC_Init(&DacHandleQ) != HAL_OK)
+  {
+    /* Initialization Error */
+    led2 = 1;
+    pc.printf(" DAC Initialization Q  ######################################### Error");
     wait(1);
     while(1){};
   }
 
   /*##-1- DAC channel1 Configuration #########################################*/
-  DACsConfig.DAC_Trigger = DAC_TRIGGER_T7_TRGO;
-  DACsConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+  DACsConfigI.DAC_Trigger = DAC_TRIGGER_T7_TRGO;
+  DACsConfigI.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+  DACsConfigQ.DAC_Trigger = DAC_TRIGGER_T7_TRGO;
+  DACsConfigQ.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
 
-  if (HAL_DAC_ConfigChannel(&DacHandle, &DACsConfig, DACx_CHANNEL) != HAL_OK)
+  if (HAL_DAC_ConfigChannel(&DacHandleI, &DACsConfigI, DACx_CHANNEL_I) != HAL_OK)
   {
     /* Channel configuration Error */
     led2 = 1;
-    pc.printf(" DAC Channel configuration  ######################################### Error");
+    pc.printf(" DAC Channel configuration I ######################################### Error");
+    wait(1);
+    while(1){};
+  }
+  if (HAL_DAC_ConfigChannel(&DacHandleQ, &DACsConfigQ, DACx_CHANNEL_Q) != HAL_OK)
+  {
+    /* Channel configuration Error */
+    led2 = 1;
+    pc.printf(" DAC Channel configuration Q  ######################################### Error");
     wait(1);
     while(1){};
   }
@@ -262,7 +292,16 @@ static void DAC_Ch1_EscalatorConfig(void)
   pc.printf(" DAC Channel configuration complete");
 
   /*##-2- Enable DAC selected channel and associated DMA #############################*/
-  if (HAL_DAC_Start_DMA(&DacHandle, DACx_CHANNEL, (uint32_t *)DataToTransmit, SYMBOLS_PERTRANSFER, DAC_ALIGN_8B_R) != HAL_OK)
+  if (HAL_DAC_Start_DMA(&DacHandleI, DACx_CHANNEL_I, (uint32_t *)DataToTransmit_I, SYMBOLS_PERTRANSFER, DAC_ALIGN_8B_R) != HAL_OK)
+  {
+    /* Start DMA Error */
+    led2 = 1;
+    pc.printf(" DAC Start DMA Error  ######################################### Error");
+    wait(1);
+    while(1){};
+  }
+
+    if (HAL_DAC_Start_DMA(&DacHandleQ, DACx_CHANNEL_Q, (uint32_t *)DataToTransmit_Q, SYMBOLS_PERTRANSFER, DAC_ALIGN_8B_R) != HAL_OK)
   {
     /* Start DMA Error */
     led2 = 1;
@@ -281,16 +320,21 @@ extern "C" {
 
     
     //has to be fucking c poes
-    void DACx_DMA_IRQHandler(void)
+    void DACx_DMA_IRQHandler_I(void)
     {
-     HAL_DMA_IRQHandler(DacHandle.DMA_Handle1);
+     HAL_DMA_IRQHandler(DacHandleI.DMA_Handle2);
     }
-
+    void DACx_DMA_IRQHandler_Q(void)
+    {
+     HAL_DMA_IRQHandler(DacHandleI.DMA_Handle1);
+    }
 
     void HAL_DAC_MspInit(DAC_HandleTypeDef *hdac)
     {
-        GPIO_InitTypeDef          GPIO_InitStruct;
-        static DMA_HandleTypeDef  hdma_dac1;
+        GPIO_InitTypeDef          GPIO_InitStructI;
+        GPIO_InitTypeDef          GPIO_InitStructQ;
+        static DMA_HandleTypeDef  hdma_dac1I;
+        static DMA_HandleTypeDef  hdma_dac1Q;
 
         /* DAC Periph clock enable */
         DACx_CLK_ENABLE();
@@ -305,38 +349,64 @@ extern "C" {
 
         /*##-2- Configure peripheral GPIO ##########################################*/
         /* DAC Channel1 GPIO pin configuration */
-        GPIO_InitStruct.Pin = DACx_CHANNEL_PIN;
-        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        HAL_GPIO_Init(DACx_CHANNEL_GPIO_PORT, &GPIO_InitStruct);
+        GPIO_InitStructI.Pin = DACx_CHANNEL_PIN_I;
+        GPIO_InitStructI.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStructI.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(DACx_CHANNEL_GPIO_PORT, &GPIO_InitStructI);
+
+                /* DAC Channel1 GPIO pin configuration */
+        GPIO_InitStructQ.Pin = DACx_CHANNEL_PIN_Q;
+        GPIO_InitStructQ.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStructQ.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(DACx_CHANNEL_GPIO_PORT, &GPIO_InitStructQ);
 
         /*##-3- Configure the DMA ##########################################*/
         /* Set the parameters to be configured for DACx_DMA_STREAM */
-        hdma_dac1.Instance = DACx_DMA_INSTANCE;
+        hdma_dac1I.Instance = DACx_DMA_INSTANCE_I;
 
-        hdma_dac1.Init.Channel  = DACx_DMA_CHANNEL;
+        hdma_dac1I.Init.Channel  = DACx_DMA_CHANNEL;
 
-        hdma_dac1.Init.Direction = DMA_MEMORY_TO_PERIPH;
-        hdma_dac1.Init.PeriphInc = DMA_PINC_DISABLE;
-        hdma_dac1.Init.MemInc = DMA_MINC_ENABLE;
-        hdma_dac1.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-        hdma_dac1.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-        //hdma_dac1.Init.Mode = DMA_NORMAL;
-               hdma_dac1.Init.Mode = DMA_CIRCULAR;
-        hdma_dac1.Init.Priority = DMA_PRIORITY_HIGH;
+        hdma_dac1I.Init.Direction = DMA_MEMORY_TO_PERIPH;
+        hdma_dac1I.Init.PeriphInc = DMA_PINC_DISABLE;
+        hdma_dac1I.Init.MemInc = DMA_MINC_ENABLE;
+        hdma_dac1I.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+        hdma_dac1I.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+        //hdma_dac1I.Init.Mode = DMA_NORMAL;
+        hdma_dac1I.Init.Mode = DMA_CIRCULAR;
+        hdma_dac1I.Init.Priority = DMA_PRIORITY_HIGH;
+        //hdma_dac1I.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
+        HAL_DMA_Init(&hdma_dac1I);
 
-        //hdma_dac1.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
 
+                /*##-3- Configure the DMA ##########################################*/
+        /* Set the parameters to be configured for DACx_DMA_STREAM */
+        hdma_dac1Q.Instance = DACx_DMA_INSTANCE_Q;
 
-        HAL_DMA_Init(&hdma_dac1);
+        hdma_dac1Q.Init.Channel  = DACx_DMA_CHANNEL;
+
+        hdma_dac1Q.Init.Direction = DMA_MEMORY_TO_PERIPH;
+        hdma_dac1Q.Init.PeriphInc = DMA_PINC_DISABLE;
+        hdma_dac1Q.Init.MemInc = DMA_MINC_ENABLE;
+        hdma_dac1Q.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+        hdma_dac1Q.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+        //hdma_dac1Q.Init.Mode = DMA_NORMAL;
+        hdma_dac1Q.Init.Mode = DMA_CIRCULAR;
+        hdma_dac1Q.Init.Priority = DMA_PRIORITY_HIGH;
+        //hdma_dac1Q.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
+         HAL_DMA_Init(&hdma_dac1I);
+        HAL_DMA_Init(&hdma_dac1Q);
+        
 
         /* Associate the initialized DMA handle to the DAC handle */
-        __HAL_LINKDMA(hdac, DMA_Handle1, hdma_dac1);
+        __HAL_LINKDMA(hdac, DMA_Handle2, hdma_dac1I);
+        __HAL_LINKDMA(hdac, DMA_Handle1, hdma_dac1Q);
 
         /*##-4- Configure the NVIC for DMA #########################################*/
         /* Enable the DMA1_Stream5 IRQ Channel */
-        HAL_NVIC_SetPriority(DACx_DMA_IRQn, 2, 0);
-        HAL_NVIC_EnableIRQ(DACx_DMA_IRQn);
+        HAL_NVIC_SetPriority(DACx_DMA_IRQn_I, 2, 0);
+        HAL_NVIC_SetPriority(DACx_DMA_IRQn_Q, 2, 0);
+        HAL_NVIC_EnableIRQ(DACx_DMA_IRQn_I);
+        HAL_NVIC_EnableIRQ(DACx_DMA_IRQn_Q);
 
     }
 
@@ -354,7 +424,8 @@ extern "C" {
 
         /*##-2- Disable peripherals and GPIO Clocks ################################*/
         /* De-initialize the DAC Channel1 GPIO pin */
-        HAL_GPIO_DeInit(DACx_CHANNEL_GPIO_PORT, DACx_CHANNEL_PIN);
+        HAL_GPIO_DeInit(DACx_CHANNEL_GPIO_PORT, DACx_CHANNEL_PIN_I);
+         HAL_GPIO_DeInit(DACx_CHANNEL_GPIO_PORT, DACx_CHANNEL_PIN_Q);
 
         // /*##-3- Disable the DMA Stream ############################################*/
         // /* De-Initialize the DMA Stream associate to DAC_Channel1 */
